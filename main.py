1c69b941cae5374a444f0b53a7e3dfe8a3d509f9
Fegen_William <fegenwilliam@gmail.com>
2024-12-10 14:36:26 +0700

    make main.py more readable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def count_char():
-    words = get_book_text()
-    lowered_words = words.lower()
+def count_char(text):
+    lowered_words = text.lower()
     count_dict = {}
     for char in lowered_words:
         if char not in count_dict:
@@ -9,18 +8,22 @@ def count_char():
             count_dict[char] += 1
     return count_dict
 
-def count_words():
-    words = get_book_text()
-    words_no_ws = words.split()
+def count_words(text):
+    words_no_ws = text.split()
     count = len(words_no_ws)
     return count
-def get_book_text():
-    with open("books/frankenstien.txt") as f:
+def get_book_text(book_path):
+    with open(book_path) as f:
         file_contents = f.read()
     return file_contents
 
 def main():
-    print(count_char())
-    count = count_words()
-    print(count)
+    book_path = "books/frankenstien.txt"
+    text = get_book_text(book_path)
+    count_word = count_words(text)
+    char_count = count_char(text)
+    print(count_word)
+    print(char_count)
+    
+    
 main()
